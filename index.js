9258d9bad31098cc850560fc1ddb8ddba1ce4b8a
revesfideles <revesfideles@gmail.com>
2024-02-24 15:13:10 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+  }
+
+  bestCustomer;
+
+  function setBestCustomer() {
+    bestCustomer = 'not bob';
+  }
+setBestCustomer();
+
+  function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+  }
+overwriteBestCustomer();
+
+const leastFavoriteCustomer = 'initial value';
+
+function changeLeastFavoriteCustomer() {
+  throw new Error('Assignment to constant variable.');
+}
+
+changeLeastFavoriteCustomer();
